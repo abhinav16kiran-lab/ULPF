05cefb32281ba0fc521cb1eed5cf13cc130ff613
adarsh.v.h <adarshvh2005@gmail.com>
2026-08-31 23:05:48 +0530

    Added source_id and linage_id to the clickhouse's raw_event table

--- infra/clickhouse-init/01_raw_events.sql
+++ infra/clickhouse-init/01_raw_events.sql
@@ -5,7 +5,9 @@ CREATE DATABASE IF NOT EXISTS ulpf_events;
 CREATE TABLE IF NOT EXISTS ulpf_raw.raw_events
 (
     event_id         String,
+    linega_id       STRING,
     vendor_id        String,
+    source_id       STRING,
     mapping_version  Nullable(UInt32),
     received_at      DateTime64(3) DEFAULT now64(3),
     raw_payload      String
